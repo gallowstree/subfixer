c6eb0db88f626d13107dc6ec8270ff9f2451e5fb
Alejandro Alvarado <alejalvarado@paypal.com>
2019-03-07 14:30:09 -0600

    [master] :)

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,43 @@ use std::fs::File;
 use std::ops::Add;
 use std::io::{BufRead, BufReader, Result, Lines};
 
-use chrono::{Timelike};
 use chrono::NaiveTime;
 use time::Duration;
+use std::io::Write;
+use std::env;
+
+const TIME_FORMAT: &str = "%H:%M:%S,%3f";
 
 fn main() -> Result<()> {
-    let file = File::open("sample.srt")?;
+    let input_file_path = env::args().next().expect("Missing input file");
+    let output_file_path = env::args().next().expect("Missing output file");
+
+    let offset_millis = env::args().next()
+        .expect("Missing offset millis")
+        .parse::<i64>()
+        .expect("Invalid offset millis");
+
+    let file = File::open(input_file_path)?;
+    let mut output_file = File::create(output_file_path)?;
     let reader = BufReader::new(file);
-    let offset_duration = Duration::milliseconds(1001);
+    let offset_duration = Duration::milliseconds(offset_millis);
 
-    let subtitle_entries = reader.lines()
-        .batching(|lines| consume_parsing_entries(lines))
-        .map(|sub_entry| sub_entry.offset_by(offset_duration))
-        .for_each(|e| println!("{:?}", e));
+    let offset_entries = reader.lines()
+        .batching(|lines| parse_subtitle(lines))
+        .map(|sub_entry| sub_entry.offset_by(offset_duration));
 
+    for e in offset_entries {
+        output_file.write_all(e.to_srt_format().as_ref()).expect("Failed to write")
+    }
 
     Ok(())
 }
 
-fn consume_parsing_entries(lines: &mut Lines<BufReader<File>>) -> Option<Subtitle> {
+fn parse_subtitle(lines: &mut Lines<BufReader<File>>) -> Option<Subtitle> {
     match lines.next() {
         Some(Ok(index_line)) => {
-            let index = parse_index(&index_line);
-            let time_marks = parse_start_end_times(&lines.next()?.unwrap());
+            let index = index_line.trim().parse::<u32>().expect("malformed file: missing index");
+            let time_marks = parse_start_end_times(&lines.next()?.expect(format!("malformed file: missing time marks at index {}", index).as_ref()));
             let subtitle_text = next_text_block(lines);
             Some(Subtitle {index, start_time: time_marks.0, end_time: time_marks.1, subtitle_text})
         },
@@ -36,19 +50,10 @@ fn consume_parsing_entries(lines: &mut Lines<BufReader<File>>) -> Option<Subtitl
     }
 }
 
-fn parse_index(line: &str) -> u32 {
-    line.trim().parse::<u32>().expect("malformed file: missing index")
-}
-
-fn parse_start_end_times(line: &str) -> (Duration, Duration) {
+fn parse_start_end_times(line: &str) -> (NaiveTime, NaiveTime) {
     line.split("-->")
         .map(|x| x.trim())
-        .map(|time_str| NaiveTime::parse_from_str(time_str, "%H:%M:%S,%f").unwrap())
-        .map(|parsed_time| {
-            let secs = parsed_time.hour() * 3600 + parsed_time.minute() * 60 + parsed_time.second();
-            let nanos = parsed_time.nanosecond() as i64;
-            Duration::seconds(secs as i64).add(Duration::nanoseconds(nanos))
-        })
+        .map(|time_str| NaiveTime::parse_from_str(time_str, TIME_FORMAT).unwrap())
         .tuple_windows::<(_, _)>()
         .next().unwrap()
 }
@@ -69,8 +74,8 @@ fn next_text_block(lines: &mut Lines<BufReader<File>>) -> Vec<String> {
 #[derive(Debug)]
 struct Subtitle {
     index: u32,
-    start_time: Duration,
-    end_time: Duration,
+    start_time: NaiveTime,
+    end_time: NaiveTime,
     subtitle_text: Vec<String>
 }
 
@@ -80,4 +85,14 @@ impl Subtitle {
         let new_end = self.end_time.add(duration);
         Subtitle {index: self.index, start_time: new_start, end_time: new_end, subtitle_text: self.subtitle_text}
     }
+
+    fn to_srt_format(&self) -> String {
+        let start = self.start_time.format(TIME_FORMAT);
+        let end = self.end_time.format(TIME_FORMAT);
+        let formatted_time_marks = format!("{} --> {}", start, end);
+        let lines = self.subtitle_text.join("\n");
+
+        format!("{}\n{}\n{}\n\n", self.index, formatted_time_marks, lines)
+    }
+
 }
